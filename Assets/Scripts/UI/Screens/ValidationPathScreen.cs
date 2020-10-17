23dfd95148adf432f085763792603c6f6aa1b489
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2020-10-17 17:04:29 +0200

    [ADD] validationPathScreen script

---
+++ Assets/Scripts/UI/Screens/ValidationPathScreen.cs
@@ -0,0 +1,23 @@
+﻿using fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Attributes;
+using fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Menu.Screens;
+using UnityEngine;
+
+namespace fr.matthiasdetoffoli.ConquestAndInfluence.UI.Screens
+{
+    /// <summary>
+    /// Screen open for validate an action
+    /// </summary>
+    public class ValidationPathScreen : AValidationScreen
+    {
+        #region Methods
+        /// <summary>
+        /// When a validation button was clicked
+        /// </summary>
+        /// <param name="pState"></param>
+        protected override void OnValidation(bool pState)
+        {
+            
+        }
+        #endregion Methods
+    }
+}
