3147c3ed689f8d31713d1c02d8a0f58b373b4249
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2020-12-12 17:31:55 +0100

    [ADD.WIP) character

---
+++ Assets/Scripts/Core/Characters/ACharacter.cs
@@ -0,0 +1,37 @@
+﻿using Fr.Matthiasdetoffoli.ConquestAndInfluence.Maps;
+using Fr.Matthiasdetoffoli.GlobalUnityProjectCode.Classes.MonoBehaviors;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Core.Characters
+{
+    /// <summary>
+    /// Abstract class for define a character
+    /// </summary>
+    public abstract class ACharacter : AMonoBehaviour
+    {
+        #region Properties
+        /// <summary>
+        /// The current path the character has to follow
+        /// </summary>
+        public List<Square> path;
+
+        /// <summary>
+        /// The current character position
+        /// </summary>
+        public Square CurrentPosition;
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// The movement of the character
+        /// </summary>
+        public virtual void Move(double pPercentOfDay)
+        {
+            if(path == null)
+            {
+
+            }
+        }
+        #endregion Methods
+    }
+}
