132fff4b12076ff031075c41c73ec1c50ed8bd9c
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2021-03-24 16:05:58 +0100

    [UPDATE] finished player movements

--- Assets/Scripts/Core/Characters/ACharacter.cs
+++ Assets/Scripts/Core/Characters/ACharacter.cs
@@ -21,11 +21,6 @@ namespace Fr.Matthiasdetoffoli.ConquestAndInfluence.Core.Characters
         /// The square where the player want to go
         /// </summary>
         private Square mTargetSquare;
-
-        /// <summary>
-        /// The percent of day we was when the character start of move
-        /// </summary>
-        private float mPercentStartOfMove;
         #endregion Fields
 
         #region Properties
@@ -48,24 +43,24 @@ namespace Fr.Matthiasdetoffoli.ConquestAndInfluence.Core.Characters
         {
             if(mTargetSquare == null && path != null && path.Count > 0)
             {
-                mPercentStartOfMove = pPercentOfDay;
                 mTargetSquare = path[0];
                 mCurrentMovement = new Vector3(mTargetSquare.position.x - currentPosition.position.x, mTargetSquare.position.y - currentPosition.position.y, transform.position.z);
             }
 
             if(mCurrentMovement != Vector3.Zero && mTargetSquare != null)
             {
-                float lCurrentPersentMove = Math.Abs(pPercentOfDay - mPercentStartOfMove);
+                float lCurrentPersentMove = Math.Abs(pPercentOfDay);
 
-                if(lCurrentPersentMove <= 1)
+                if(lCurrentPersentMove < 1)
                 {
-                    transform.position.Set(currentPosition.position.x + mCurrentMovement.X * lCurrentPersentMove, currentPosition.position.y + mCurrentMovement.Y * lCurrentPersentMove, transform.position.z);
-                    path.RemoveAt(0);
+                    transform.position = new UnityEngine.Vector3(currentPosition.position.x + mCurrentMovement.X * lCurrentPersentMove, currentPosition.position.y + mCurrentMovement.Y * lCurrentPersentMove, transform.position.z);
                 }
                 else
                 {
                     transform.position.Set(mTargetSquare.position.x, mTargetSquare.position.y, transform.position.z);
                     mCurrentMovement = Vector3.Zero;
+                    path.RemoveAt(0);
+                    currentPosition = mTargetSquare;
                     mTargetSquare = null;
                 }
             }
