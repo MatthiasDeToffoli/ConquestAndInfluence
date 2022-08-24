bb0965bac0d12cca6843f34bf898e4d618e9bdd7
Matthias de Toffoli <matthias.detoffoli@gmail.com>
2022-08-24 17:48:53 +0200

    [QUALITY] remove useless property

--- Assets/Scripts/UI/Screens/MainScreen.cs
+++ Assets/Scripts/UI/Screens/MainScreen.cs
@@ -6,27 +6,5 @@ namespace Fr.Matthiasdetoffoli.ConquestAndInfluence.UI.Screens
 {
     public class MainScreen : AMenuScreen
     {
-        #region Fields
-        /// <summary>
-        /// The multi button
-        /// </summary>
-        [SerializeField]
-        private SwitchScreenButtonListener mMultiButton;
-        #endregion Fields
-
-        #region Methods
-        /// <summary>
-        /// Start of the behaviour
-        /// </summary>
-        protected override void Start()
-        {
-            base.Start();
-
-            if (mMultiButton != null)
-            {
-                mMultiButton.Isinteractable = false;
-            }
-        }
-        #endregion Methods
     }
 }
